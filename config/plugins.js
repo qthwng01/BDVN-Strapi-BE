782f650cc7c9428b25f3b91823af0e61d2a2d1ed
qthwng01 <you@example.com>
2024-07-17 12:52:31 +0700

    fix file upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,15 @@
 module.exports = ({ env }) => ({
   // ...
+  upload: {
+    config: {
+      provider: "local",
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+    },
+  },
+  // ...
+  // ...
   seo: {
     enabled: true,
   },
